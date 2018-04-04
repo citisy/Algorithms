c5c903185a4d339d8affd710a51e276e1b924a62
citisy <34095119+citisy@users.noreply.github.com>
2018-04-04 21:44:01 +0800

    init

---
+++ graph/Graph.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# 图 G = (V, E)
+# V => 顶点集
+# E => 边集
+
+
+class Graph():
+    '''
+    function:
+
+    '''
+    def __init__(self):
+        pass
+
+    def init(self):
+        pass
+
+    def traverse(self):
+        pass
+
+    def find(self):
+        pass
+
+    def print_(self):
+        pass
+
+    def minspan(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def topol(self):
+        pass
+
+    def key(self):
+        pass
---
+++ graph/Matrix.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# 邻接矩阵：表示图中顶点之间相邻关系的矩阵。
+# A[i, j] => 1 => 存在
+#            0 => 不存在
+
+
+class Matrix():
+    def __init__(self):
+        pass
+
+    def init(self):
+        pass
